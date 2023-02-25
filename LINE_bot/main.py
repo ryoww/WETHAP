36f7c16360094dba4c97098b4f467fe91283d5a6
adelppi <adelppi.baig@gmail.com>
2023-02-25 14:15:56 +0900

    APIサーバの基本形作った

--- LINE_bot/main.py
+++ LINE_bot/main.py
@@ -1,2 +1,32 @@
-import flask
-print("ok")
+from flask import *
+
+app = Flask(__name__)
+
+
+# クエリがない状態、動作確認用に
+@app.route("/", methods = ["GET"])
+def root():
+
+    return {"status": "online"}
+
+    #例: http://127.0.0.1:8000
+
+
+# 実際に情報を返すAPI
+@app.route("/info/", methods = ["GET"])
+def returnInfo():
+
+    # クエリパラメータの取得
+    lab = str(request.args.get("lab"))
+    data = {
+        "lab": lab
+    }
+    return data
+
+    #例: http://127.0.0.1:8000/info/?lab=test
+
+
+if __name__ == "__main__":
+
+    # APIサーバを8000番ポートで起動
+    app.run(port = 8000)
