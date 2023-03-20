d7cac1939bf0c131d931e19d0bf62345e66889c0
adelppi <adelppi.baig@gmail.com>
2023-03-21 00:28:36 +0900

    URL修正

--- LINE_bot/fetch_info.py
+++ LINE_bot/fetch_info.py
@@ -2,7 +2,7 @@ import requests
 import json
 from datetime import datetime
 
-API_BASE_URL = "http://192.168.3.2/getInfo"
+API_BASE_URL = "https://adelppi.duckdns.org/getInfo"
 
 def fetchInfo(location: str, date: str, num_gen: int):
     inputURL = f"{API_BASE_URL}/?location={location}&year={datetime.now().year}&month={date.split('/')[0]}&day={date.split('/')[1]}&num_gen={num_gen}"
