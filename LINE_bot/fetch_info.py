494b780b5e0c1864e87db8ac2076825473b85add
adelppi <adelppi.baig@gmail.com>
2023-03-21 00:27:24 +0900

    クエリでfetchするようにした

--- LINE_bot/fetch_info.py
+++ LINE_bot/fetch_info.py
@@ -2,16 +2,16 @@ import requests
 import json
 from datetime import datetime
 
-API_BASE_URL = "http://localhost/getInfo"
+API_BASE_URL = "http://192.168.3.2/getInfo"
 
 def fetchInfo(location: str, date: str, num_gen: int):
-    inputURL = API_BASE_URL + "/?location=高専&year=2023&month=03&day=16&num_gen=8"
+    inputURL = f"{API_BASE_URL}/?location={location}&year={datetime.now().year}&month={date.split('/')[0]}&day={date.split('/')[1]}&num_gen={num_gen}"
     response = requests.get(inputURL)
     fetchedData = json.loads(response.text)
 
     return fetchedData
 
 if __name__ == "__main__":
-    infoDict= fetchInfo("下沢家", "3/16", 8)
+    infoDict= fetchInfo("下沢家", "03/16", 8)
 
     print(infoDict)
