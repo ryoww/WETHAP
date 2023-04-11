dd2e539d2ebf6e466e6f6f53b1c32ec789c418fb
ryoww <ryo.shimozawa.0121@gmail.com>
2023-04-11 10:05:19 +0900

    とりあえずサブミット

--- LINE_bot/app.py
+++ LINE_bot/app.py
@@ -17,6 +17,7 @@ from key import at, sk
 
 import pprint
 import re
+import requests
 from datetime import datetime
 from datetime import date
 from fetch_info import fetchInfo
@@ -70,11 +71,8 @@ endTime = {
     12: "19:30"
     }
 
-# lower limit date
-mindate = datetime(2023,3,16).date()
-
-# rooms list
-rooms = ["下沢家","テスト研究室"]
+# # lower limit date
+# mindate = datetime(2023,3,16).date()
 
 
 # process initial situation
@@ -84,44 +82,44 @@ def initial_state(event):
 
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text))
     # change situation submitted location
-    return STATE_LOCATION_RECEIVED
+    update_id_dict(event.source.user_id,state = STATE_LOCATION_RECEIVED)
 
 
 # process after submitted location
 def location_received_state(event, location):
 
-    if location in rooms:
-        text = (f"{location}の情報を知りたい日付と実験時間を教えてください。(例:4月1日4限)")
+    isRegistered = requests.get(f"https://adelppi.duckdns.org/isRegistered/?labID={location}").text
 
+    if isRegistered == "True":
+        update_id_dict(event.source.user_id,state = STATE_DATE_RECEIVED,location = location)
+
+
+        text = (f"{location}の情報を知りたい日付と実験時間を教えてください。(例:4月1日4限)")
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text))
-        # change initial situation  
-        return STATE_DATE_RECEIVED
 
     # if not registered location "plrease retype"
     else:
         text = "登録されている実験室又は研究室を入力ください。"
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text))
 
-        return STATE_LOCATION_RECEIVED
-
 
 id_dict = {}
 
 def update_id_dict(id,**update_data):
-    if id in id_dict:
+    if id in id_dict.keys():
         for key, value in update_data.items():
             id_dict[id][key] = value
     else:
         print(f"{id} not found")
 
 def delete_data(id):
-    if id in id_dict:
+    if id in id_dict.keys():
         del id_dict[id]
     else:
         print(f"{id} not found")
 
 def add_data(id,**new_data):
-    if id in id_dict:
+    if id in id_dict.keys():
         print(f"{id} already exists")
     else:
         id_dict[id] = new_data
@@ -143,21 +141,30 @@ def handle_message(event):
     nowtime = now.time()
 
     pprint.pprint(event)
+    print(event)
     print(event.message.text)
+    print(event.source.user_id)
     # print(event)
 
-    if "教え" in event.message.text:
-        # set initial state
-        state = STATE_INITIAL
-        location = ""
-        state = initial_state(event)
+    print(id_dict)
 
-    elif state == STATE_LOCATION_RECEIVED:
+    if "教え" in event.message.text:
+        # set inital state and register id
+        if str(event.source.user_id) not in id_dict.keys():
+            add_data(event.source.user_id,state = STATE_INITIAL,location = "")
+
+        # set initial state
+        initial_state(event)
+
+
+    # elif id_dict.get(event.source.user_id,dict()).get(state) == STATE_LOCATION_RECEIVED:
+    elif str(event.source.user_id) in id_dict.keys() and id_dict[event.source.user_id]["state"] == STATE_LOCATION_RECEIVED:
         # get location
         location = event.message.text
-        state = location_received_state(event, location)
+        location_received_state(event, location)
 
-    elif state == STATE_DATE_RECEIVED:
+    # elif state == STATE_DATE_RECEIVED:
+    elif str(event.source.user_id) in id_dict.keys() and id_dict[event.source.user_id]["state"] == STATE_DATE_RECEIVED:
         # try :
             # get day
             if "月" in event.message.text and "日" in event.message.text and "限" in event.message.text:
@@ -171,6 +178,7 @@ def handle_message(event):
 
                 print(date)
                 endtime = datetime.strptime(endTime[num_gen],"%H:%M").time()
+                update_id_dict(event.source.user_id,date = date)
 
 
                 if 1 > num_gen < 12:
@@ -181,25 +189,24 @@ def handle_message(event):
                     text = "いつからWETHAPが未来を観測できると錯覚していた？もう一度入力しやがれ！！"
                     line_bot_api.reply_message(event.reply_token,TextSendMessage(text))
 
-                elif date < mindate:
-                    text = "データがありません"
-                    line_bot_api.reply_message(event.reply_token,TextSendMessage(text))
+                # elif date < mindate:
+                #     text = "データがありません"
+                #     line_bot_api.reply_message(event.reply_token,TextSendMessage(text))
 
                 elif date == now_day and endtime > nowtime:
                     text = "反映までもう少々お待ちください。"
                     line_bot_api.reply_message(event.reply_token,TextSendMessage(text))
 
                     # set initial state
-                    state = STATE_INITIAL
-                    location = ""
-
+                    delete_data(event.source.user_id)
 
                 else:
                         # 4/1~submit
                         # if 3 >= now.month >= 1 and 12 >= date.month >= 4:
                         #     date.replace(year =  now.year - 1)
 
-
+                        location = str(id_dict[event.source.user_id]["location"])
+                        print(location)
                         info = fetchInfo(location, date, num_gen)
                         T = info["temperature"]
                         H = info["humidity"]
@@ -209,8 +216,8 @@ def handle_message(event):
                         line_bot_api.reply_message(event.reply_token,TextSendMessage(text))
 
                         # set initial state
-                        state = STATE_INITIAL
-                        location = ""
+                        delete_data(event.source.user_id)
+                        print(id_dict)
 
 
             else :
