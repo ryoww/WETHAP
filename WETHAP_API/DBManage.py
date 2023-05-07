121b90a83468ea9ddedf819ff33e5dee6e88d98c
adelppi <adelppi.baig@gmail.com>
2023-05-07 13:52:31 +0900

    cache削除

---
+++ WETHAP_API/DBManage.py
@@ -0,0 +1,88 @@
+import sqlite3
+
+class DBCommands:
+    def __init__(self, DB_PATH):
+        self.DB_PATH = DB_PATH
+        self.connection = sqlite3.connect(self.DB_PATH)
+        self.cursor = self.connection.cursor()
+
+
+    def createDB(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS infos(labID TEXT, date DATE, numGen INTEGER, temperature INTEGER, humidity INTEGER, pressure INTEGER, weather TEXT)")
+
+
+    def insert(
+        self,
+        labID: str,
+        date: str,
+        numGen: int,
+        temperature: int,
+        humidity: int,
+        pressure: int,
+        weather: str
+    ):
+        self.cursor.execute(f"INSERT INTO infos VALUES('{labID}', '{date}', {numGen}, {temperature}, {humidity}, {pressure}, '{weather}')")
+
+
+    def previewData(self):
+        self.cursor.execute("SELECT * FROM infos")
+        response = self.cursor.fetchall()
+
+        return response
+
+
+    def isRegistered(
+        self,
+        labID: str
+    ):
+        self.cursor.execute(f"SELECT DISTINCT labID FROM infos WHERE labID = '{labID}'")
+        response = self.cursor.fetchall()
+        
+        if response:
+            return True
+        else:
+            return False
+
+
+    def select(
+        self,
+        date: str,
+        labID: str,
+        numGen: int
+    ):
+        self.cursor.execute(f"SELECT * FROM infos WHERE labID = '{labID}' and date = '{date}' and numGen = {numGen}")
+        data = self.cursor.fetchall()
+        return data[0] if data else None
+
+
+    def close(self):
+        self.cursor.close()
+        self.connection.commit()
+        self.connection.close()
+
+
+if __name__ == "__main__":
+    # DBへ接続
+    db = DBCommands(DB_PATH = "./data.db")
+
+    # WETHAP用のDBテーブルのテンプレートを作る
+    db.createDB()
+
+    # 登録されている研究室か
+    print(db.isRegistered("テスト研究室"))
+
+    # データを挿入する
+    db.insert(
+        labID = "テスト研究室",
+        date = "1970-1-1",
+        numGen = 1,
+        temperature = 0,
+        humidity = 0,
+        pressure = 0,
+        weather = "晴れ")
+
+    # 条件に合ったデータを、レコードをタプルとした配列で取得
+    print(db.select(labID = "テスト研究室", date = "1970-1-1", numGen = 1))
+
+    # DBの状態を保存、DBの接続を終了する
+    db.close()
---
+++ WETHAP_API/postdata.py
@@ -0,0 +1,15 @@
+import requests
+
+URL = "http://127.0.0.1:8000/addInfo"
+
+postInfo = {
+    "labID": "テスト研究室",
+    "numGen": 1,
+    "temperature": 0,
+    "humidity": 0,
+    "pressure": 0
+}
+
+response = requests.post(URL, json = postInfo)
+
+print(response)
