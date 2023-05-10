8bd3d26c222f959bfe0ef1f5bca085ac6244b72d
solithv <120559288+solithv@users.noreply.github.com>
2023-05-10 22:00:45 +0900

    optimize

--- Pico/main.py
+++ Pico/main.py
@@ -19,11 +19,9 @@ try:
     display = ssd1306.SSD1306_I2C(128, 64, disp_i2c)
 except:
     is_display = False
-    loop_time = 3
 else:
-    # ssd1306接続判定用
+    # ssd1306接続フラグ
     is_display = True
-    loop_time = 5
 if is_display:
     display.fill(0)
     display.text("WETHAP", 40, 0)
@@ -43,11 +41,11 @@ try:
     bme_i2c = I2C(0, sda=Pin(0), scl=Pin(1))
     time.sleep_ms(100)
     bme = BME680_I2C(bme_i2c)
-except Exception as err:
+except Exception as error:
     if is_display:
         display.text("bme680 error", 16, 11)
         display.show()
-    raise err
+    raise error
 else:
     if is_display:
         display.text("bme680 connect", 8, 11)
@@ -55,56 +53,52 @@ else:
 
 
 # 定数定義
-labID = "テストT"
+lab_id = "テストT"
 url = "https://adelppi.duckdns.org/addInfo"
-finish_time = ["09:25","10:10", "11:10", "11:55", "13:30", "14:15", "15:15", "16:00", "17:00", "17:45", "18:45", "19:30", "20:48"]
+finish_time = ("09:25","10:10", "11:10", "11:55", "13:30", "14:15", "15:15", "16:00", "17:00", "17:45", "18:45", "19:30", "20:48")
 
-ssid = '************'
-password = '********'
+ssid = "************"
+password = "********"
 
 # wifi初期化
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
-if wlan.active():
-    if is_display:
-        display.text("wifi activate", 8, 22)
-        display.text("connecting...", 12, 33)
-        display.show()
-else:
-    if is_display:
-        display.text("wifi error", 24, 22)
-        display.show()
-    raise Exception
+if is_display:
+    display.text("wifi activate", 8, 22)
+    display.text("connecting...", 12, 33)
+    display.show()
 # wifi接続待機
-max_wait = 60
+max_wait = 30
 for i in range(max_wait):
     time.sleep(1)
     if wlan.isconnected():
+        print("online")
+        if is_display:
+            display.text("wifi connect", 14, 44)
+            display.show()
         break
-if i < max_wait - 1:
-    if is_display:
-        display.text("wifi connect", 14, 44)
-        display.show()
 else:
+    print("offline")
     if is_display:
-        display.text("offline", 36, 44)
+        display.text("connect faild", 12, 44)
         display.show()
-    raise Exception
-
+    raise Exception("WiFi could't connect")
 
 rtc = machine.RTC()
 timZone = 9
 ntptime.host = "ntp.nict.jp"
-# 時計初回更新フラグ
+# 起動時初期化完了フラグ
 is_init = False
 
+# ポストフラグ
+is_post = False
 
 while True:
-    # 時計更新
-    # 起動後更新成功するまでと1時間に1回更新実行
     t0 = machine.RTC().datetime()
-    if (not is_init) or (t0[5] == 0 and t0[6] == 0):
+
+    # 内部時計更新
+    if not is_init or (t0[5] == 0 and t0[6] == 0):
         try:
             ntptime.settime()
         except:
@@ -116,6 +110,7 @@ while True:
         else:
             t0 = machine.RTC().datetime()
             is_init = True
+            print("initialize complete")
             if is_display:
                 display.fill(0)
                 display.text("time update", 20, 24)
@@ -138,13 +133,13 @@ while True:
     print(f"{t0[0]}-{t0[1]}-{day} {hour}:{t0[5]}:{t0[6]}")
 
     # 取得データ表示
-    # NOTE: 此処の処理でループ時間約1秒増加してる
+    # NOTE: 此処の処理でループ時間約2秒増加
     if is_display:
         display.fill(0)
         display.show()
         display.text(f"{t0[0]}-{t0[1]:02d}-{day:02d}", 24, 0)
         time_disp = f"{hour:02d}:{t0[5]:02d}:{t0[6]:02d}"
-        display.text(time_disp, (128-len(time_disp)*8)//2, 10)
+        display.text(f"{hour:02d}:{t0[5]:02d}:{t0[6]:02d}", 32, 10)
         display.hline(0, 20, 128, 1)
         display.text(f"Temp:{bme.temperature:.3g}C", 24, 22)
         display.text(f"Hmd.:{bme.humidity:.3g}%", 24, 33)
@@ -157,9 +152,10 @@ while True:
     sec = t0[6]
     # print(sec)
 
-    if is_init and nowtime in finish_time and sec in range(loop_time):
+    if is_init and nowtime in finish_time and not is_post:
+        is_post = True
         data = {
-            "labID": labID,
+            "labID": lab_id,
             "date": f"{t0[0]}-{t0[1]:02d}-{day:02d}",
             "numGen": int(finish_time.index(nowtime)) + 1,
             "temperature": f"{bme.temperature:.2f}",
@@ -167,32 +163,34 @@ while True:
             "pressure": f"{bme.pressure:.2f}",
         }
 
-        print(data["labID"])
-
+        led.off()
         response = urequests.post(url, data=json.dumps(data).encode("unicode_escape"), headers={"Content-Type": "application/json"})
 
-        print(f"{response.status_code}")
-        print(f"{response.content}")
+        print(data["labID"])
+        print(response.status_code)
+        print(response.content)
 
         if is_display:
             if response.status_code == 200:
                 display.fill(0)
-                display.text('post:succeed', 8, 0)
+                display.text("post:succeed", 8, 0)
                 display.hline(0, 9, 128, 1)
                 display.text(f'date:{data["date"]}', 4, 11)
-                display.text(f'numGen:{data["numGen"]}', (128-len(str(data["numGen"]))*8-56)//2, 22)
-                display.text(f'Temp:{data["temperature"]}C', (128-len(data["temperature"])*8-48)//2, 33)
-                display.text(f'Hmd.:{data["humidity"]}%', (128-len(data["humidity"])*8-48)//2, 44)
-                display.text(f'Pres.:{data["pressure"]}hPa', (128-len(data["pressure"])*8-72)//2, 55)
+                display.text(f'numGen:{data["numGen"]}', (72-len(str(data["numGen"]))*8)//2, 22)
+                display.text(f'Temp:{data["temperature"]}C', (80-len(data["temperature"])*8)//2, 33)
+                display.text(f'Hmd.:{data["humidity"]}%', (80-len(data["humidity"])*8)//2, 44)
+                display.text(f'Pres.:{data["pressure"]}hPa', (56-len(data["pressure"])*8)//2, 55)
                 display.show()
             else:
                 display.fill(0)
                 display.text("post failed", 20, 24)
-                display.text(f"with {response.status_code}", 32, 32)
+                display.text(f"with {response.status_code}", (88-len(str(response.status_code))*8)//2, 32)
                 display.show()
 
-        led.off()
         time.sleep(1)
         led.on()
+
+    if is_post and nowtime not in finish_time:
+        is_post = False
 
     time.sleep(1)
