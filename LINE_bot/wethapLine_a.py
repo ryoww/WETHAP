982a703f55eef0bd8b1903a09497c3ec6419ede8
ryoww <ryo.shimozawa.0121@gmail.com>
2023-09-16 03:09:07 +0900

    update

--- LINE_bot/wethapLine_a.py
+++ LINE_bot/wethapLine_a.py
@@ -3,14 +3,17 @@ import requests
 import json
 import pprint
 
+
 from datetime import datetime
 from datetime import date
 from fetch_info import fetchInfo
+
 
 # set situation
 STATE_INITIAL = 0
 STATE_LOCATION_RECEIVED = 1
 STATE_DATE_RECEIVED = 2
+
 
 # end time
 endTime = {
@@ -30,7 +33,6 @@ endTime = {
 
 
 def load_json():
-
     with open("data.json", "r") as file:
         data = json.load(file)
     return data
@@ -41,24 +43,54 @@ def write_json(data):
         json.dump(data, file, indent=4)
 
 
-def register_data(id, state, location, num_gen):
+def delete_json(id):
     data = load_json()
 
-    new_data = {id : {
-                        "state" : state,
-                        "location" : location,
-                        "num_gen" : num_gen
-                }}
-
-    data.update(new_data)
+    del data[id]
 
     write_json(data)
 
+def register_data(id, **new_data):
+    data = load_json()
 
-def initial_step(event):
-    text = "情報を知りたい研究室又は実験室教えてください"
+    if id in data.keys():
+        return "already exisits"
 
-    return 
+    else :
+        data[id] = new_data
+
+        write_json(data)
+
+
+def update_data(id, **new_data):
+    data = load_json()
+
+    if id in data.keys():
+        for key, value in new_data.items():
+            data[id][key] = value
+
+        write_json(data)
+
+
+# process after submitted location
+def location_received_state(event, location):
+
+    # request API
+    isRegisterd = requests.get(f"https://adelppi.duckdns.org/isRegistered/?labID={location}").text
+
+    if isRegisterd == "True":
+        update_data(event.source.user_id, state = STATE_DATE_RECEIVED, location = location)
+
+        text = (f"{location}の情報を知りたい日付と実験時間を教えてください。(例:4月1日4限)")
+
+        return text
+
+    # if not registered location, reply "please retype location"
+    else:
+        text = "登録されている実験室又は研究室を入力ください。"
+
+        return text
+
 
 def handle_text(event):
     # set time
@@ -70,8 +102,17 @@ def handle_text(event):
 
     # if "教え" in message, register id
     if "教え" in event.message.text:
+
         # set initial state and register id
         if str(event.source.user_id) not in user_dict.keys():
-            register_data(event.source.user_id, STATE_INITIAL, "")
+            register_data(event.source.user_id, state = STATE_INITIAL, location = "")
 
-        initial_step(event)
+        update_data(event.source.user_id, state = STATE_LOCATION_RECEIVED)
+
+        return "情報を知りたい研究室又は実験室教えてください"
+
+
+    elif user_dict[event.source.user_id]["state"] == STATE_LOCATION_RECEIVED:
+        text = location_received_state(event.source.user_id, event.message.text)
+
+        return text
