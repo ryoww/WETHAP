e28618c8745613f083e48275623bb68501d11131
solithv <120559288+solithv@users.noreply.github.com>
2023-12-18 08:44:12 +0900

    add env sample

---
+++ Pico/env_sample.py
@@ -0,0 +1,12 @@
+LAB_ID: str = "***"
+
+API_SERVER: str = "ws://***"
+TIME_SERVER: str = "https://worldtimeapi.org/api/ip"
+NTP_SERVER: str = "ntp.nict.jp"
+
+SSID: str = "***"
+PASSWORD: str = "***"
+WIFI_DELAY: int = 200
+ATTEMPTS: int = 3
+
+SOCKET_DELAY: int = 5
