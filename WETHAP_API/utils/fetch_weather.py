eae3766d5d3c1f7d2c448c1ab8a4ba825dfc2cb8
solithv <120559288+solithv@users.noreply.github.com>
2024-05-07 14:05:49 +0900

    fix: fetch weather error handling

--- WETHAP_API/utils/fetch_weather.py
+++ WETHAP_API/utils/fetch_weather.py
@@ -3,11 +3,14 @@ from bs4 import BeautifulSoup
 
 
 def fetch_weather():
-    r = requests.get(
-        "https://weathernews.jp/onebox/35.731350/139.798464/q=%E5%8D%97%E5%8D%83%E4%BD%8F%EF%BC%88%E6%9D%B1%E4%BA%AC%E9%83%BD%EF%BC%89&v=e8be546f5505407d1788791e7e7b3b0c15fbfd38af41f3dab5a6d2b88cb74d84&temp=c&lang=ja"
-    )
-    soup = BeautifulSoup(r.text, "html.parser")
-    return soup.find(class_="nowWeather").find(name="figcaption").text
+    try:
+        r = requests.get(
+            "https://weathernews.jp/onebox/35.731350/139.798464/q=%E5%8D%97%E5%8D%83%E4%BD%8F%EF%BC%88%E6%9D%B1%E4%BA%AC%E9%83%BD%EF%BC%89&v=e8be546f5505407d1788791e7e7b3b0c15fbfd38af41f3dab5a6d2b88cb74d84&temp=c&lang=ja"
+        )
+        soup = BeautifulSoup(r.text, "html.parser")
+        return soup.find(class_="nowWeather").find(name="figcaption").text
+    except Exception:
+        return "Error"
 
 
 if __name__ == "__main__":
